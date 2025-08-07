00474cff8f02f72084c8ce56612d27318df88739
Kiran Nirmal <nirmalkiran20@gmail.com>
2025-08-07 15:44:27 +0530

    deleated new folder

--- app/components/BentoCard.tsx
+++ app/components/BentoCard.tsx
@@ -15,18 +15,22 @@ interface BentoCardProps {
 export function BentoCard({
   children,
   height = "h-auto",
-  rowSpan = 8,
-  colSpan = 7,
+  rowSpan,
+  colSpan,
   className = "",
   showHoverGradient = true,
   hideOverflow = true,
   linkTo,
 }: BentoCardProps) {
+  const spanClasses = `${rowSpan ? `row-span-${rowSpan}` : ""} ${
+    colSpan ? `col-span-${colSpan}` : ""
+  }`;
+
   const cardContent = (
     <div
       className={`group relative flex flex-col rounded-2xl border border-border-primary bg-bg-primary p-6 hover:bg-white ${
-        hideOverflow && "overflow-hidden"
-      } ${height} row-span-${rowSpan} col-span-${colSpan} ${className}`}
+        hideOverflow ? "overflow-hidden" : ""
+      } ${height} ${spanClasses} ${className}`}
     >
       {linkTo && (
         <div className="absolute bottom-4 right-4 z-[999] flex h-9 w-9 rotate-6 items-center justify-center rounded-full bg-indigo-200 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-y-[-8px] group-hover:rotate-0 group-hover:opacity-100">
@@ -54,9 +58,11 @@ export function BentoCard({
           </svg>
         </div>
       )}
+
       {showHoverGradient && (
-        <div className="user-select-none pointer-events-none absolute inset-0 z-30 bg-gradient-to-tl from-indigo-400/20 via-transparent to-transparent opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100"></div>
+        <div className="pointer-events-none absolute inset-0 z-30 user-select-none bg-gradient-to-tl from-indigo-400/20 via-transparent to-transparent opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
       )}
+
       {children}
     </div>
   );
